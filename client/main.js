57f38baf0b787c37e687aaed521b9edb3eb8fd98
Estefania Sosa <estefi.monumental@gmail.com>
2022-08-30 10:06:46 -0400

    Modal working

--- client/main.js
+++ client/main.js
@@ -27,19 +27,19 @@ function createHikeCard() {
                   <p>${elem.location_name}, ${elem.state}</p>                
                   <h3>${elem.hike_name}</h3>
                 </div></div>  
-                              
+
                 <!-- Button trigger modal -->
-                <div class="btn-div"><button type="button" class="btn-modal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <div class="btn-div"><button type="button" class="btn-modal" data-bs-toggle="modal" data-bs-target="#exampleModal${elem.hike_id}">
                   Select
                 </button></div>
 
                 <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal fade" id="exampleModal${elem.hike_id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                   <div class="modal-dialog">
                     <div class="modal-content">
                       <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel"></h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                       </div>
                       <div class="modal-body">
                         <p>Difficulty: ${elem.hike_dificulty}</p>
